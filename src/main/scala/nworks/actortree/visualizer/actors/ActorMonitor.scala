622ebfdff78cf540cbbfb5a5da2c2af1d7374275
Markus Jura <markus.jura@gmail.com>
2014-11-15 16:05:02 +0100

    Use actor path instead of name
    Signed-off-by: Markus Jura <markus.jura@gmail.com>

--- src/main/scala/nworks/actortree/visualizer/actors/ActorMonitor.scala
+++ src/main/scala/nworks/actortree/visualizer/actors/ActorMonitor.scala
@@ -11,7 +11,7 @@ class ActorMonitor extends Actor {
 
   implicit val writes = new Writes[NewActorCreated] {
     override def writes(o: NewActorCreated): JsValue =
-      Json.obj("actorpath" -> o.ref.path.name, "event" -> Json.obj("type" -> "started"))
+      Json.obj("actorpath" -> o.ref.path.address.toString, "event" -> Json.obj("type" -> "started"))
   }
 
   def receive = {
@@ -23,7 +23,7 @@ class ActorMonitor extends Actor {
 
 
     case Terminated(ref) =>
-      val json = Json.stringify(Json.obj("actorpath" -> ref.path.name, "event" -> Json.obj("type" -> "terminated")))
+      val json = Json.stringify(Json.obj("actorpath" -> ref.path.address.toString, "event" -> Json.obj("type" -> "terminated")))
       Kafka.producer.sendMessage(json)
       println(">>>> actor is removed with name " + ref.path.name)
 
